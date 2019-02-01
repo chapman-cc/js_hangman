7ffcf16eab721bc6cbb6feb6c0c28a287cd786a6
chapmanCC <chapboy1989@gmail.com>
2019-02-02 00:03:52 +0800

    add event listener to .btn__reset

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //Attach a event listener to the “Start Game” button to hide the start screen overlay.
+    const resetButton = document.querySelector('.btn__reset');
+    resetButton.addEventListener('click', (e) => {
+        const overlay = resetButton.parentNode;
+        overlay.style.display = 'none'
+    })
     //================================================================================
 });
