f5620e6e0dda650b3a407d8583c76e8fadc57ed6
chapmanCC <chapboy1989@gmail.com>
2019-02-04 03:49:51 +0800

    minor adjustment

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
 // document.addEventListener('DOMContentLoaded', () => {
 
-    // Press "Start Game" button to remove overlay <div>
-    const overlayDiv = document.querySelector('#overlay');
-    overlayDiv.addEventListener('click', (e) => {
-        if (e.target.className === 'btn__reset') {
-            overlayDiv.style.display = 'none';
-        }
-    });
-
-    // list of possible phrase for game
-    const phrases = ['I love you', 'Never say never', 'Hashtag', 'Go For Broke', 'Swinging For the Fences', 'On the Same Page', 'sunday'];
-    const ul = document.querySelector('#phrase ul');
-    const qwerty = document.querySelector('#qwerty')
-    const scoreBoardImg = document.querySelectorAll('#scoreboard img')
-    let winScore = 0;
-    let missedScore = 0;
-
-    //     Return a random phrase in array e.g. ["t", "u", "e", "s", "d", "a", "y"]
-    const getRandomPhraseAsArray = (array) => {
-        let randomNum = Math.round(Math.random() * (array["length"] - 1));
-        let randomWord = array[randomNum].toUpperCase(); //e.g. 'TUESDAY'
-        let phraseArr = [];
-        for (let i = 0; i < randomWord.length; i++) {
-            phraseArr.push(randomWord[i]);
-        }
-        return phraseArr;
-    };
-
-    const addPhrasetoDisplay = (array) => {
-        for (let i = 0; i < array.length; i++) {
-            let li = document.createElement('li');
-            li.textContent = array[i];
-            if (array[i] == " ") {
-                li.className = 'space';
-            } else {
-                li.className = 'letter';
-            }
-            ul.appendChild(li);
-        }
+// Press "Start Game" button to remove overlay <div>
+const overlayDiv = document.querySelector('#overlay');
+overlayDiv.addEventListener('click', (e) => {
+    if (e.target.className === 'btn__reset') {
+        overlayDiv.style.display = 'none';
     }
+});
 
-    const phraseArray = getRandomPhraseAsArray(phrases);
-    addPhrasetoDisplay(phraseArray);
+// list of possible phrase for game
+const phrases = ['I love you', 'Never say never', 'Hashtag', 'Go For Broke', 'Swinging For the Fences', 'On the Same Page', 'sunday'];
+const ul = document.querySelector('#phrase ul');
+const qwerty = document.querySelector('#qwerty')
+const scoreBoardImg = document.querySelectorAll('#scoreboard img')
+let winScore = 0;
+let missedScore = 0;
+
+//     Return a random phrase in array
+const getRandomPhraseAsArray = (array) => {
+    let randomNum = Math.round(Math.random() * (array["length"] - 1));
+    let randomWord = array[randomNum].toUpperCase();
+    let phraseArr = [];
+    for (let i = 0; i < randomWord.length; i++) {
+        phraseArr.push(randomWord[i]);
+    }
+    return phraseArr;
+};
+
+const addPhrasetoDisplay = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = array[i];
+        if (array[i] == " ") {
+            li.className = 'space';
+        } else {
+            li.className = 'letter';
+        }
+        ul.appendChild(li);
+    }
+}
+
+let phraseArray = getRandomPhraseAsArray(phrases);
+addPhrasetoDisplay(phraseArray);
 
 //================================================================================
 
@@ -51,20 +51,22 @@ const checkLetter = (letter) => {
     const list = ul.children;
     let isCorrect = false;
     for (li of list) {
-        if (li.className === 'letter' && li.textContent === letter.toUpperCase() ) {
+        if (li.className === 'letter' && li.textContent === letter.toUpperCase()) {
             li.className = 'show';
             isCorrect = true;
         }
     }
-    if (isCorrect) {return letter;}
+    if (isCorrect) {
+        return letter;
+    }
 };
 
 const checkWin = () => {
     winScore += 1;
     const list = ul.children;
-    let isLetter = false
+    let isLetter = false;
     for (li of list) {
-        li.className === 'letter' ? isLetter = true : isLetter = false;
+        li.className === 'letter' ? isLetter = true : null;
     }
     if (!isLetter) {
         resetGame('win')
